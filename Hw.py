fb2e181ad5db4b072773f6414aae1340f21fe8e8
arjun-dotcom1256 <sriiarjun@gmail.com>
2026-06-20 17:38:56 +0530

    Add files via upload

--- Hw.py
+++ Hw.py
@@ -9,5 +9,8 @@ elif mood.lower() == "bad":
     print("I'm sorry your day has been bad. Think of something that would get your mind off it, like watching a movie or playing a video game. I hope the rest of your day is going to be great")
 else:
     print("That's okay, but your day can't be neutral, mid, or okay. It should be great. Do something you always wanted to do")
-
-
+end=input("Do you want to continue talking?")
+if end.lower() =="yes":
+    print("Great, I am happy to continue talking with you")
+else:
+    print("Okay, I hope you have a great day")
